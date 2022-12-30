5260f6149aed479fcd811689f9b3c9501c6c7c52
Karanvir Singh Sangha <iamkaransangha@gmail.com>
2022-12-29 16:42:02 -0800

    Update app.py

--- src/app.py
+++ src/app.py
@@ -6,7 +6,9 @@ app = Flask(__name__)
 @app.route("/")
 def main():
     return '''
+     <h1>Twitter Analyzer</h1>
      <form action="/echo_user_input" method="POST">
+         <label for="user_input">Enter any text - </label>
          <input name="user_input">
          <input type="submit" value="Submit!">
      </form>
@@ -16,4 +18,4 @@ def main():
 @app.route("/echo_user_input", methods=["POST"])
 def echo_input():
     input_text = request.form.get("user_input", "")
-    return "You entered: " + input_text
+    return "<h1>Thank you for using the form!</h1><p>You entered: " + input_text + "</p>"
